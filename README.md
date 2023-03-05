ee9d828803e939002b45bb1f5c27b60a2dac8972
jlxip <jlxip@protonmail.com>
2023-03-05 14:34:12 +0100

    First version

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # tg2h
-Trivial Gemini to HTTP proxy
+This is a really simple proxy from Gemini to HTTP.
+
+Its configuration is given via two environment variables:
+- `TG2H_ADDR` contains the remote Gemini server. For example: `gemini://jlxip.net`.
+- `TG2H_STYLE` contains the URL (can be relative!) to a generic stylesheet. Defaults to an empty value, so no style is used.
+
+tg2h will send files as-is, with their given MIME type in the `Content-Type` value, for all formats except `text/gemini`, which it translates to HTML. The only thing to note is that the `<title>` is set to the first heading text (`# hello` -> `hello`) if it's the first line of the file.
+
+Want to give it a look? [jlxip.net](https://jlxip.net) uses it.
---
+++ src/main.rs
@@ -0,0 +1,155 @@
+use gmi::{gemtext::GemtextNode, protocol::StatusCode, url::Url};
+use std::env;
+use warp::{self, http::Response, hyper::Body, Filter};
+
+#[macro_use]
+extern crate lazy_static;
+
+// TODO: Consider also formatting "plain/text"
+// TODO: Redirects
+// TODO: Inputs
+// TODO: Serve favicon?
+
+lazy_static! {
+    static ref URL: String = env::var("TG2H_URL").unwrap();
+    static ref STYLE: String = env::var("TG2H_STYLE").unwrap_or("".to_string());
+}
+
+#[tokio::main]
+async fn main() {
+    let filter = warp::any()
+        .and(warp::path::full())
+        .map(|p: warp::path::FullPath| proxy(p.as_str()));
+
+    warp::serve(filter).run(([0, 0, 0, 0], 8080)).await;
+}
+
+macro_rules! no_good {
+    ($expre: expr) => {
+        return Response::builder()
+            .status(500)
+            .body(Body::from(format!("TG2H: {}", $expre)))
+            .unwrap();
+    };
+}
+
+// Get Gemini response
+fn proxy(path: &str) -> warp::reply::Response {
+    let url = format!("{}{}", &*URL, path.to_string());
+    let Ok(url) = Url::try_from(url.as_str()) else {
+        no_good!(format!("bad url: {}", url));
+    };
+
+    let Ok(resp) = gmi::request::make_request(&url) else {
+        no_good!("could not make request to server");
+    };
+
+    match resp.status {
+        StatusCode::Input(_) => {
+            no_good!(format!("input not supported: {}", resp.meta));
+        }
+        StatusCode::Success(_) => { /* ok! */ }
+        StatusCode::Redirect(_) => {
+            no_good!(format!("redirect not supported: {}", resp.meta));
+        }
+        // Anything else is an actual error
+        x => {
+            no_good!(format!("error: {:?}: {}", x, resp.meta));
+        }
+    };
+
+    let mime = resp.meta;
+    let raw = resp.data;
+
+    // Gemtext?
+    if mime == "text/gemini" {
+        // Convert to HTML
+        let Ok(text) = String::from_utf8(raw) else {
+            no_good!("invalid UTF-8 character in text/gemini mime type");
+        };
+        Response::builder()
+            .status(200)
+            .header("Content-Type", "text/html; charset=utf-8")
+            .body(Body::from(gem2html(&text)))
+            .unwrap()
+    } else {
+        // Not gemtext, return it in its content type
+        Response::builder()
+            .status(200)
+            .header("Content-Type", mime)
+            .body(Body::from(raw))
+            .unwrap()
+    }
+}
+
+macro_rules! clean {
+    ($expre: expr) => {
+        html_escape::encode_text($expre)
+    };
+}
+
+// Gemtext to HTML
+fn gem2html(gem: &str) -> String {
+    // Construct the body first
+    let nodes = gmi::gemtext::parse_gemtext(gem);
+    let mut body = String::new();
+    for i in nodes {
+        let line = match i {
+            GemtextNode::Text(x) => format!("<p>{}</p>", clean!(&x)),
+            GemtextNode::Link(x, c) => {
+                let c = match c {
+                    Some(k) => k,
+                    None => x.clone(),
+                };
+                format!("<a href=\"{}\">{}</a><br>", clean!(&x), clean!(&c))
+            }
+            GemtextNode::Heading(x) => format!("<h1>{}</h1>", clean!(&x)),
+            GemtextNode::SubHeading(x) => format!("<h2>{}</h2>", clean!(&x)),
+            GemtextNode::SubSubHeading(x) => format!("<h3>{}</h3>", clean!(&x)),
+            GemtextNode::ListItem(x) => format!("<li>{}</li>", clean!(&x)),
+            GemtextNode::Blockquote(x) => format!("<pre>{}</pre>", clean!(&x)),
+            GemtextNode::Preformatted(x, _) => format!("<pre>{}</pre>", clean!(&x)),
+            GemtextNode::EmptyLine => "<br>".to_string(),
+        } + "\n";
+        body += &line;
+    }
+
+    // Do we have a title?
+    let mut title = String::new();
+    if let Some(o) = gem.lines().next() {
+        if o.len() > 2 {
+            let (lhs, rhs) = o.split_at(2);
+            if lhs == "# " {
+                title = format!("<title>{}</title>", rhs);
+            }
+        }
+    }
+
+    // Do we have a style?
+    let style = if STYLE.len() > 0 {
+        format!("<link rel=\"stylesheet\" href=\"{}\">", *STYLE)
+    } else {
+        "".to_string()
+    };
+
+    // Wrap it in a nice HTML file with a header, and that's it
+    format!(
+        "
+<!DOCTYPE HTML>
+
+<HTML>
+    <head>
+        <meta charset=\"UTF-8\">
+        <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\" />
+        {}
+        {}
+    </head>
+    <body>
+        <div id='outer'><div id='middle'><div id='inner'>
+        {}
+        </div></div></div>
+    </body>
+</HTML>",
+        title, style, body
+    )
+}
